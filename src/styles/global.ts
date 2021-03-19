b298fe2b3bb4d21099d1634b10439077c45a48b6
Gabriel Gigante <gabriel_gigante@outlook.com>
2021-03-18 21:54:01 -0300

    style: setup global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,7 +2,16 @@ import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
   :root {
-    --background: #f8f2f5;
+    --red: #E52E4D;
+    --blue: #5429CC;
+
+    --blue-light: #6933FF;
+
+    --text-title: #363F5F;
+    --text-body: #969CB3;
+
+    --background: #F0F2F5;
+    --shape: #FFFFFF;
   }
   
   * {
@@ -11,8 +20,27 @@ export const GlobalStyles = createGlobalStyle`
     box-sizing: border-box;
   }
 
+  html {
+    @media(max-width: 1080px) {
+      font-size: 93.75%;
+    }
+
+    @media(max-width: 720px) {
+      font-size: 87.5%;
+    }
+  }
+
   body {
     background: var(---background);
     -webkit-font-smoothing: antialiased;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
   }
 `;
