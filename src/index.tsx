f3de61da34206ef5aa1e582535b8d0584d2c0afa
Gabriel Gigante <gabriel_gigante@outlook.com>
2021-03-21 10:41:39 -0300

    feat: listing transactions

--- src/index.tsx
+++ src/index.tsx
@@ -9,16 +9,39 @@ createServer({
     transaction: Model
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelancer de website',
+          type: 'deposit',
+          category: 'Desenvolvimento',
+          amount: 5000,
+          createdAt: new Date('2021-02-12 9:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguél',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1100,
+          createdAt: new Date('2021-02-17 11:00:00'),
+        },
+      ],
+    });
+  },
+
   routes() {
     this.namespace = 'api';
 
-    this.get('/transactions', () => this.schema.all('transaction'))
+    this.get('/transactions', () => this.schema.all('transaction'));
 
     this.post('/transactions', (schema, request) => {
       const data = JSON.parse(request.requestBody);
 
       return schema.create('transaction', data);
-    })
+    });
   }
 });
 
